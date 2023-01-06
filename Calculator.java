b7180cdbddb36aec4e9de52d6c0d9879b02a7a5d
stephenharrity <sjh401@gmail.com>
2023-01-05 19:14:36 -0600

    calculator

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+public class Calculator {
+  // building basic calculator
+  public Calculator(){
+
+  }
+
+  public int add(int a, int b){
+    return b + a;
+  }
+
+  public int subtract(int a, int b){
+    return a - b;
+  }
+
+  public int multiply(int a, int b){
+    return a * b;
+  }
+
+  public int divide(int a, int b){
+    return a / b;
+  }
+
+  public int modulo(int a, int b){
+    return a % b;
+  }
+
+  public static void main(String[] args){
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5,7));
+    System.out.println(myCalculator.subtract(45,11));
+    System.out.println(myCalculator.multiply(5,7));
+    System.out.println(myCalculator.divide(44,11));
+  }
+}
