9ea4655c464bad5f6a578f618c511b9b64a72779
stephenharrity <sjh401@gmail.com>
2023-01-07 20:40:48 -0600

    classroom

--- Classroom.java
+++ Classroom.java
@@ -5,7 +5,6 @@ public class Classroom {
   public static void main(String[] args){
     String[] students = {"Sade", "Alexus", "Sam", "Koma"};
     double[] mathScores = new double[4];
-    
     mathScores[0] = 94.5;
     mathScores[2] = 76.8;
 
